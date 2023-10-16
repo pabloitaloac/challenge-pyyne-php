e94e26fd850d9de343e25603bd5922f33cbb01ab
Pablo Ítalo de Araújo Carvalho <dev@pabloitalo.com>
2023-10-16 15:06:41 -0300

    Improving the form view and adding a stylish touch to successfully sent forms

--- Sample.php
+++ Sample.php
@@ -1,5 +1,7 @@
 <?php
 
+include('base.html');
+
 require 'Form.php';
 require 'Input.php';
 require 'TextInput.php'; 
@@ -8,17 +10,33 @@ $form = new Form();
 $form->addInput(new TextInput("firstname", "First Name:", "Bruce"));
 $form->addInput(new TextInput("lastname", "Last Name:", "Wayne"));
 
+$content = '';
+
 if ($_SERVER['REQUEST_METHOD']=="POST") {
+    // Form sent
     if ($form->validate()) {
         $firstName = $form->getValue("firstname");
         $lastName = $form->getValue("lastname");
-        echo $firstName." ".$lastName;
+
+
+        // Successful validation, respond with an HTML message
+        $content = '<div class="form-body" >
+            <div class="success-show"><h1>Form successfully submitted. 🎉</h1><div class="result"><h3>You sent:</h3><p>' . $firstName . ' ' . $lastName . '</p></div></div>
+        </div>';
     } else {
-        $form->display();
+        // Validation fail
+        $content = $form->display();
     }
 } else {
-    $form->display();
+    // Form not sent yet
+    $content = $form->display();
 }
+
+
+echo '<script>';
+echo 'document.getElementById("content").innerHTML = ' . json_encode($content) . ';';
+echo '</script>';
+
 
 
 
